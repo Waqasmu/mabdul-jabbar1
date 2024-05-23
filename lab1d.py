8b62aebff8992972418b882ec712185f25b5c1d6
Waqasmu <mw8513951@gmail.com>
2024-05-23 18:24:38 -0400

    added lab1d.

---
+++ lab1d.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+# Define the objects x, y, and z
+x = 10
+y = 2
+z = 5
+
+# Calculate the result of the expression
+result = x + y * z
+
+# Print the required output
+print(f"{x} + {y} * {z} = {result}")
+
